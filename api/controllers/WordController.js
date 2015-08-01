d492d036586efaa5df94962fe9616db83391aeb3
Julian <julian.deocampo@gmail.com>
2015-08-01 01:22:29 -0500

    Some server/client validation, bootstrap, toastr.js

--- api/controllers/WordController.js
+++ api/controllers/WordController.js
@@ -9,33 +9,45 @@
  	fetchwords: function(req, res) {
  		Word.find().exec(function(err, words) {
  			if(err) {return res.json(err.status, {err: err})}
- 			else{
- 				return res.json(words)
- 			}
- 		})
+ 				else{
+ 					return res.json(words)
+ 				}
+ 			})
  	},
 
  	submit: function(req, res) {
- 		Word.create({
+ 		// Check for uniqueness of word in wordbank
+ 		Word.find({
  			name: req.body.name,
- 			score: 0,
  			isInBook: false
- 		}).exec(function(err, word) {
- 			if (err) {
- 				return res.json(err.status, {err: err})
+ 		}).exec(function duplicateWord(err, word){
+ 			if(word.length > 0){
+ 				return res.status(406).end();
  			}
- 			if (word) {
- 				res.status(200).end()
- 				return
+ 			else{
+ 				Word.create({
+ 					name: req.body.name,
+ 					score: 0,
+ 					isInBook: false
+ 				}).exec(function(err, word) {
+ 					if (err) {
+ 						return res.json(err.status, {err: err})
+ 					}
+ 					if (word) {
+ 						res.status(200).end()
+ 						return
+ 					}
+ 				})
  			}
- 		})
+ 		}
+ 		)
  	},
 
  	vote: function(req, res) {
  		var newScore = 0;
  		Word.find({name: req.body.name}).exec(function foundWord(err, word){
  			if(err) {return res.json(err.status, {err: err})}
- 			wordToUpdate = word[0]
+ 				wordToUpdate = word[0]
  			if (req.body.vote == 1){
  				wordToUpdate.score++
  			}
@@ -59,6 +71,6 @@
  					return res.status(200).end()
  				}
  			})
-		})
-	}
-}
+ 		})
+ 	}
+ }
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,62 +3,60 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
-  	  
-      $http.get('/fetchwords').then(function(resp){
-          console.log('Succcess', resp)
-          $scope.words = resp.data
-        }, function(err){
-          console.log(err)
-        })
-      
-  	  $scope.addWord = function() {
-        $http.post('/submit', {
-          name: $scope.name
-        })
-        .then(function onSuccess(sailsResponse){
-          console.log("Success!")
-        })
-        .catch(function onError(sailsResponse){
-          console.log("Ruh-roh raggy!")
-        })
+controller('MyCtrl1', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
-  	  	if ($scope.name || $scope.name != '') {
-  	  		$scope.words.push({
-  	  			name: $scope.name,
-  	  			isInBook: false,
-  	  			score: 0
-  	  		})
-  	  		$scope.name=''
-  	  	}
-  	  }
+  $http.get('/fetchwords').then(function(resp){
+    $scope.words = resp.data
+  }, function(err){
+    console.log(err)
+  })
 
-  	  $scope.clickVote = function(word) {
-        word.score += 1;
-        $http.post('/vote', {
-          name: word.name,
-          vote: 1
+  $scope.addWord = function() {
+    $http.post('/submit', {
+      name: $scope.name
+    })
+    .then(function onSuccess(sailsResponse){
+      console.log("Success!")
+      if ($scope.name || $scope.name != '') {
+        $scope.words.push({
+          name: $scope.name,
+          isInBook: false,
+          score: 0
         })
-        .then(function onSuccess(sailsResponse){
-          console.log("Success!")
-        })
-        .catch(function onError(sailsResponse){
-          console.log("Ruh-roh raggy!")
-        })
+        $scope.name=''
       }
+    })
+    .catch(function onError(sailsResponse){
+      toastr.error("Could not add word!")
+    })
+  }
 
-    /* CLOCK */
+  $scope.clickVote = function(word) {
+    word.score += 1;
+    $http.post('/vote', {
+      name: word.name,
+      vote: 1
+    })
+    .then(function onSuccess(sailsResponse){
+      console.log("Success!")
+    })
+    .catch(function onError(sailsResponse){
+      toastr.error("Could not send vote!")
+    })
+  }
+
+  /* CLOCK */
     $scope.clock = "Loading Timer..."; // initialise the time variable
     $scope.tickInterval = 1000 //ms
     var tick = function() {
         $scope.clock = Date.now() // get the current time
         $timeout(tick, $scope.tickInterval); // reset the timer
-    }
+      }
     // Start the timer
     $timeout(tick, $scope.tickInterval);
 
   }])
-  
-  .controller('MyCtrl2', [function($scope) {
-  }])
+
+.controller('MyCtrl2', [function($scope) {
+}])
 ;
